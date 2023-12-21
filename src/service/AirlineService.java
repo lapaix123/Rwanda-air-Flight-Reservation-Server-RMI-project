76596654efba5a26d79007f769a15151b285b255
la paix <dj.lapaix.tech@gmail.com>
2023-12-21 04:44:45 +0200

    same change to implementation

---
+++ src/service/AirlineService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+import model.Airline;
+
+/**
+ *
+ * @author la paix
+ */
+public interface AirlineService extends Remote{
+    String createAirline(Airline airline) throws RemoteException;
+
+    String updateAirline(Airline airline) throws RemoteException;
+
+    String deleteAirline(Airline airline) throws RemoteException;
+
+    List<Airline> getAllAirlines() throws RemoteException;
+
+    Airline findAirlineById(Long airlineId) throws RemoteException;
+}
--- src/service/FlightService.java
+++ src/service/FlightService.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package service.implementation;
+package service;
 
 /**
  *
  * @author la paix
  */
-public interface PassengerService {
+public interface FlightService {
     
 }
